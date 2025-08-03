06f20dd70b37f4b20b14b47c20dcb89c8c4e1467
Abhijit Kumar <abhijitkrm@gmail.com>
2025-08-03 23:07:27 +0530

    Update walletWhitelist.ts

--- config/walletWhitelist.ts
+++ config/walletWhitelist.ts
@@ -1,6 +1,5 @@
 // List of whitelisted wallet addresses (lowercase, checksum, or mixed; comparison will be case-insensitive)
 export const WALLET_WHITELIST: string[] = [
   '0x26E76B18D4A132A9397C46af11e4688BDB602E92',
-  '0xabcdefabcdefabcdefabcdefabcdefabcdefabcd',
   '0xA7aB2BdE765F177B4393374d5d2d35E6eeD536cB',
 ];
